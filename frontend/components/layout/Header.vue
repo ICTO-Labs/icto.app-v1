b28985139771b89198890b872542552fd0c0c526
Fern Nguyen <vietlinhqv@gmail.com>
2024-01-29 11:40:11 +0700

    Refactor menu links in Header.vue

--- frontend/components/layout/Header.vue
+++ frontend/components/layout/Header.vue
@@ -26,17 +26,17 @@ const router = useRoute();
 									<div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
 										<!--begin::Menu-->
 										<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
-											<div data-kt-menu-dismiss="true" :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='my-token' || router.name=='token-detail'?'here':''}`">
+											<div :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='my-token' || router.name=='token-detail'?'here':''}`">
 												<router-link class="menu-link py-3" to="/my-token">
 													<span class="menu-title">My Tokens</span>
                                                 </router-link>
 											</div>
-											<div data-kt-menu-dismiss="true" :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='contract' || router.name=='contract-detail' ?'here':''}`">
+											<div :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='contract' || router.name=='contract-detail' ?'here':''}`">
 												<router-link class="menu-link py-3" to="/payments">
 													<span class="menu-title">Payments</span>
                                                 </router-link>
 											</div>
-											<div data-kt-menu-dismiss="true" class="menu-item menu-lg-down-accordion me-lg-1">
+											<div class="menu-item menu-lg-down-accordion me-lg-1">
 												<router-link class="menu-link py-3" to="/vesting">
 													<span class="menu-title">Vesting</span>
                                                 </router-link>
@@ -97,7 +97,7 @@ const router = useRoute();
 													</div>
 												</div>
 											</div>
-											<div data-kt-menu-dismiss="true" :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='launchpad' || router.name=='new-launchpad' ?'here':''}`">
+											<div :class="`menu-item menu-lg-down-accordion me-lg-1 ${router.name=='launchpad' || router.name=='new-launchpad' ?'here':''}`">
 												<router-link class="menu-link py-3" to="/launchpad">
 													<span class="menu-title">Launchpad</span>
                                                 </router-link>
